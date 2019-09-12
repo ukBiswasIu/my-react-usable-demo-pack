1329a876aefff6de099629b19572b82a936367b8
Syncrhonous <syncmac@Syncrhonouss-Mac-mini.local>
2019-09-12 15:28:01 +0600

    new string added

--- src/__tests__/Greeter.test.ts
+++ src/__tests__/Greeter.test.ts
@@ -1,4 +1,8 @@
-import { Greeter } from '../index';
+import { Greeter, YourName } from '../index';
 test('My Greeter', () => {
   expect(Greeter('Carl')).toBe('Hello Carl');
+});
+
+test('My Greeter', () => {
+  expect(YourName('Carl')).toBe('Your name is: Carl');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export const Greeter = (name: string) => `Hello ${name}`;
+export const YourName = (name: string) => `Your name is: ${name}`;
