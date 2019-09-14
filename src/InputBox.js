5f1509e09848cd73b18673b47bcbf21dbf55ff62
Syncrhonous <syncmac@Syncrhonouss-Mac-mini.local>
2019-09-14 12:33:18 +0600

    changes

--- src/InputBox.js
+++ src/InputBox.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 export default class InputBox extends Component {
-  // hello(name) {
-  //   return `Hey ${name}`;
-  // }
-
-  render() {
-    return <div>hello new div</div>;
+  hello(name) {
+    return `Hey ${name}`;
   }
+
+  // render() {
+  //   return <div>hello new div</div>;
+  // }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-const MyComponent = require('./MyComponent');
-//import MyComponent from './MyComponent';
-// import InputBox from './InputBox';
+import InputBox from './InputBox';
 // import { Component } from 'react';
 
-//export const Greeter = name => `Hello ${name}`;
-//export const YourName = name => `Your name is: ${name}`;
+export const Greeter = name => `Hello ${name}`;
+export const YourName = name => `Your name is: ${name}`;
 //export default InputBox;
-// export {InputBoxer};
+ export {InputBoxer};
 //export const InputBox = new InputBox();
-//my.hello('Stack Overflow');
-
-export { MyComponent };
+//my.hello('Stack Overflow');s
