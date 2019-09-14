6e0b56d25559d135c4f089c4fce200a34aad34b1
Syncrhonous <syncmac@Syncrhonouss-Mac-mini.local>
2019-09-14 12:05:13 +0600

    changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import InputBox from './InputBox';
 // import { Component } from 'react';
 
-export const Greeter = name => `Hello ${name}`;
-export const YourName = name => `Your name is: ${name}`;
-export const InputBox;
+//export const Greeter = name => `Hello ${name}`;
+//export const YourName = name => `Your name is: ${name}`;
+export default InputBox;
 // export {InputBoxer};
 //export const InputBox = new InputBox();
 //my.hello('Stack Overflow');
