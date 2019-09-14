0aa6ea2f0a3620f9135d6df1224008d4a6b215be
Syncrhonous <syncmac@Syncrhonouss-Mac-mini.local>
2019-09-14 11:50:54 +0600

    changes

--- src/InputBox.js
+++ src/InputBox.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class InputBox {
+export default class InputBox extends Component {
   // hello(name) {
   //   return `Hey ${name}`;
   // }
